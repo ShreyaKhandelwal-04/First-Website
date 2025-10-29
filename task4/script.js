7f8a4da89bcbddd28db2fb6362a2492303709d34
ShreyaKhandelwal-04 <shreya4306@email.com>
2025-10-29 17:57:00 +0530

    Removed task4 folder from GitHub

--- task4/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("response").textContent = "Thank you! We'll get back to you soon 😊";
-  this.reset();
-});
